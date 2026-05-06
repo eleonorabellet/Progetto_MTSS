69717b421f08c3e2ecf34ad801ea3f8e71520ad3
RavUnipd <162745021+RavUnipd@users.noreply.github.com>
2026-05-06 17:44:55 +0200

    Risolto #2 e #3: Setup Checkstyle e GitHub Actions

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Ravenna 2111930
+// Eleonora Bellet 2111002
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Ravenna 2111930
+// Eleonora Bellet 2111002
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
